7ef7334a5c796b037d347e1d9b1e375af20a5a99
dbristow <dbristow@us.ibm.com>
2018-07-10 20:30:16 -0600

    Updated simulator to use mac als clienId

--- RAILS/rfidsim/org/rails/kjcrr/PubSync.java
+++ RAILS/rfidsim/org/rails/kjcrr/PubSync.java
@@ -5,11 +5,12 @@ import org.eclipse.paho.client.mqttv3.*;
 public class PubSync {
 
 	public static void main(String[] args) {
-		String clientId = "dbristow" ;
-		//{"mac":"4c:eb:42:bd:69:51","sensor":"rfid01","rfid":"a12345677a"}
+		String clientId = null;
+		//{"mac":"4ceb42bd6951","sensor":"rfid01","rfid":"a12345677a"}
 
 		String msg = "{\"mac\":\"" + args[0] + "\",\"sensor\":\"" + args[1] + "\",\"rfid\":\"" + args[2] + "\"}";
 		try {
+			clientId = args[0];
 			MqttClient client = new MqttClient("tcp://"+args[3]+":1883" , clientId);
 			MqttTopic topic = client.getTopic("sensors/rfid");
 			System.out.println(msg);
