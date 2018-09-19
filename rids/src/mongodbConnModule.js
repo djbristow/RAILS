a19457b56443323c2a3d68419a9c53377dde41d8
dbristow <dbristow@us.ibm.com>
2018-09-18 18:34:25 -0600

    dockerizing RIDS

--- rids/src/mongodbConnModule.js
+++ rids/src/mongodbConnModule.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 module.exports.connect = function() {
-	mongoose.connect('mongodb://localhost/rails');
+	mongoose.connect('mongodb://' + process.env.MONGODB_PORT_27017_TCP_ADDR + ':' + process.env.MONGODB_PORT_27017_TCP_PORT + '/rails');
 	var db = mongoose.connection;
 	db.on("error", console.error.bind(console, "connection error"));
 	db.once("open", function(callback){
