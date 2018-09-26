cf66678d71cb86abecf73b8df25e36bd899e0736
dbristow <dbristow@us.ibm.com>
2018-09-26 13:09:41 -0600

    Dockerized RSRM

--- rsrm/src/main.js
+++ rsrm/src/main.js
@@ -2,7 +2,7 @@ import Vue from 'vue'
 import App from './App'
 import router from './router'
 import Buefy from 'buefy'
-import 'buefy/lib/buefy.css'
+import 'buefy/dist/buefy.css'
 
 Vue.config.productionTip = false
 Vue.use(Buefy)
