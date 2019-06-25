afafa3da9e696cad0c4d96d08655e4974398fd24
dbristow <dbristow@us.ibm.com>
2019-06-25 07:53:00 -0400

    upgrade to Webpack 4

---
+++ mrim/src/index.js
@@ -0,0 +1,15 @@
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+import VueSelect from 'vue-select'
+
+Vue.config.productionTip = false
+Vue.use(Buefy)
+Vue.component('v-select', VueSelect)
+new Vue({
+  el: '#app',
+  router,
+  render: h => h(App)
+}).$mount('#app')
