383d9e38eeccb57fb9f7de5703648c94548d030e
djbristow <dave@bristow-family.org>
2019-07-17 06:42:52 -0400

    Update app.js

--- rids/src/app.js
+++ rids/src/app.js
@@ -1,10 +1,8 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
-//const morgan = require('morgan')
 
 const app = express()
-//app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
